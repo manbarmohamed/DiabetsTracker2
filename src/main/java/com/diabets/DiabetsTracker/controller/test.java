03e18424947fb913a05a7f98ca859fd9db81c96a
cherkaoui oussama <oussamacherkaoui2019@gmail.com>
2024-06-12 15:24:09 +0100

    affihcer conseil de certain glycemie

--- src/main/java/com/diabets/DiabetsTracker/controller/test.java
+++ src/main/java/com/diabets/DiabetsTracker/controller/test.java
@@ -1,14 +1,27 @@
 package com.diabets.DiabetsTracker.controller;
 
+import com.diabets.DiabetsTracker.services.AdviceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("api/test/")
 public class test {
+    @Autowired
+    private AdviceService adviceService;
+
     @GetMapping("/")
     public String show(){
         return "index";
+    }
+
+    @GetMapping("/advice")
+    public String showAdvice(ModelMap model){
+        model.put("advice",adviceService.getAdviceByGlucoseReading(1));
+        return "advice";
     }
 }
--- src/main/java/com/diabets/DiabetsTracker/services/AdviceService.java
+++ src/main/java/com/diabets/DiabetsTracker/services/AdviceService.java
@@ -1,4 +1,20 @@
 package com.diabets.DiabetsTracker.services;
 
+import com.diabets.DiabetsTracker.model.Advice;
+import com.diabets.DiabetsTracker.repository.AdviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
 public class AdviceService {
+    @Autowired
+    private AdviceRepository adviceRepository;
+
+    @Transactional
+    public Advice getAdviceByGlucoseReading(Integer level) {
+        return adviceRepository.findByGlucoseReading(level);
+    }
 }
